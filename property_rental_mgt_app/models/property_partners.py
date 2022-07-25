a01e00ca42684c463f34f5930eb1fca159e72f24
LAHORETS <77092775+LAHORETS@users.noreply.github.com>
2022-07-25 12:14:31 +0500

    a

--- property_rental_mgt_app/models/property_partners.py
+++ property_rental_mgt_app/models/property_partners.py
@@ -3,35 +3,37 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
-class ResPartner(models.Model):
-	_inherit = 'res.partner'
 
-	partner_type = fields.Selection([('renter','Renter'),('purchaser','Purchaser'),('other','Other')])
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    partner_type = fields.Selection([('renter', 'Tenant'), ('purchaser', 'Purchaser'), ('other', 'Other')])
+
 
 class ResUser(models.Model):
-	_inherit = 'res.users'
+    _inherit = 'res.users'
 
-	@api.model
-	def create(self, vals):
-		res = super(ResUser, self).create(vals)
-		renter = res.has_group('property_rental_mgt_app.group_rent_payer')
-		purchaser = res.has_group('property_rental_mgt_app.group_purchaser')
+    @api.model
+    def create(self, vals):
+        res = super(ResUser, self).create(vals)
+        renter = res.has_group('property_rental_mgt_app.group_rent_payer')
+        purchaser = res.has_group('property_rental_mgt_app.group_purchaser')
 
-		if renter:
-			res.partner_id.write({'user_id':res.id, 'partner_type':'renter'})
-		if purchaser:
-			res.partner_id.write({'user_id':res.id, 'partner_type':'purchaser'})
-		return res
+        if renter:
+            res.partner_id.write({'user_id': res.id, 'partner_type': 'renter'})
+        if purchaser:
+            res.partner_id.write({'user_id': res.id, 'partner_type': 'purchaser'})
+        return res
 
-	def write(self, vals):
-		res = super(ResUser, self).write(vals)
-		renter = self.env.ref('property_rental_mgt_app.group_rent_payer')
-		purchaser = self.env.ref('property_rental_mgt_app.group_purchaser')
+    def write(self, vals):
+        res = super(ResUser, self).write(vals)
+        renter = self.env.ref('property_rental_mgt_app.group_rent_payer')
+        purchaser = self.env.ref('property_rental_mgt_app.group_purchaser')
 
-		if renter:
-			if self.has_group('property_rental_mgt_app.group_rent_payer'):
-				self.partner_id.write({'user_id':self.id, 'partner_type':'renter'})
-		if purchaser:
-			if self.has_group('property_rental_mgt_app.group_purchaser'):
-				self.partner_id.write({'user_id':self.id, 'partner_type':'purchaser'})
-		return res
+        if renter:
+            if self.has_group('property_rental_mgt_app.group_rent_payer'):
+                self.partner_id.write({'user_id': self.id, 'partner_type': 'renter'})
+        if purchaser:
+            if self.has_group('property_rental_mgt_app.group_purchaser'):
+                self.partner_id.write({'user_id': self.id, 'partner_type': 'purchaser'})
+        return res
--- property_rental_mgt_app/models/property_reserve.py
+++ property_rental_mgt_app/models/property_reserve.py
@@ -2,28 +2,35 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-from datetime import date,datetime
+from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
+
 
 class PropertyBook(models.TransientModel):
     _name = 'property.book'
     _description = "Reserve Rental Property"
 
-    contract_id = fields.Many2one('contract.contract',string="Choose Contract", required=True, help="Default expired and renewal date get by start(from) date for one month contract.")
-    property_id = fields.Many2one('product.product', required=True, domain=[('is_property','=',True)])
+    contract_id = fields.Many2one('contract.contract', string="Choose Contract", required=True,
+                                  help="Default expired and renewal date get by start(from) date for one month contract.")
+    property_id = fields.Many2one('product.product', required=True, domain=[('is_property', '=', True)])
     desc = fields.Text()
     rent_price = fields.Float()
-    renter_id = fields.Many2one('res.users','Renter')
+    renter_id = fields.Many2one('res.users', 'Renter')
     owner_id = fields.Many2one('res.partner')
     deposite = fields.Float(string="Monthly Rent", required=True)
     from_date = fields.Date(string="From Date", required=True)
-    to_date = fields.Date(string="Expired Date", required=True, help="Default expired date automatically set for one month after start date. ")
+    to_date = fields.Date(string="Expired Date", required=True,
+                          help="Default expired date automatically set for one month after start date. ")
     renewal_date = fields.Date("Renew Date", required=True)
-    state = fields.Selection([('avl','Available'),('reserve','Reserve')], string="Status", default='avl')
-    contract_month = fields.Integer("Contract Months", help="Rent Contract months calculate base selected contract(monthly/yearly) type.")
+    state = fields.Selection([('avl', 'Available'), ('reserve', 'Reserve')], string="Status", default='avl')
+    contract_month = fields.Integer("Contract Months",
+                                    help="Rent Contract months calculate base selected contract(monthly/yearly) type.")
     deposite_amount = fields.Float()
-    total_deposite  = fields.Float("Total Rent")
+    total_deposite = fields.Float("Total Rent")
     month = fields.Integer()
+    # payment_terms = fields.Selection(
+    #     [('annualy', 'Annualy'), ('biannualy', 'Biannualy'), ('quarterly', 'Quarterly'), ('monthly', 'Monthly'),
+    #      ('weekly', 'Weekly')])
 
     @api.onchange('contract_id')
     def get_month_deposite(self):
@@ -36,15 +43,14 @@ class PropertyBook(models.TransientModel):
             self.total_deposite = self.deposite_amount
             self.month = self.contract_month
 
-
-    @api.onchange('from_date','contract_id')
+    @api.onchange('from_date', 'contract_id')
     def check_contract_date(self):
         if self.from_date and self.property_id:
             if self.from_date < date.today() or self.property_id.property_avl_from > self.from_date:
                 return {
-                        'warning': {'title': 'Warning!', 'message': 'Please enter valid contract start date...!'},
-                        'value': {'from_date': None}
-                        }
+                    'warning': {'title': 'Warning!', 'message': 'Please enter valid contract start date...!'},
+                    'value': {'from_date': None}
+                }
 
             if self.contract_id:
                 if self.contract_id.contract_type == 'monthly':
@@ -55,17 +61,17 @@ class PropertyBook(models.TransientModel):
 
     # get rent Property details.
     @api.model
-    def default_get(self,default_fields):
+    def default_get(self, default_fields):
         res = super(PropertyBook, self).default_get(default_fields)
         ctx = self._context
         property_data = {
-            'deposite_amount':ctx.get('deposite'),
-            'property_id':ctx.get('property_id'),
-            'desc':ctx.get('desc'),
-            'rent_price':ctx.get('rent_price'),
-            'renter_id':ctx.get('renter_id'),
-            'owner_id':ctx.get('owner_id'),
-            'deposite':ctx.get('deposite')
+            'deposite_amount': ctx.get('deposite'),
+            'property_id': ctx.get('property_id'),
+            'desc': ctx.get('desc'),
+            'rent_price': ctx.get('rent_price'),
+            'renter_id': ctx.get('renter_id'),
+            'owner_id': ctx.get('owner_id'),
+            'deposite': ctx.get('deposite')
         }
         res.update(property_data)
         return res
@@ -75,26 +81,33 @@ class PropertyBook(models.TransientModel):
             state = 'running'
         else:
             state = 'new'
-        contract_id = self.env['contract.details'].create({'contract_month':self.month,'deposite':self.deposite_amount,'renewal_date':self.renewal_date,'rent_price':self.property_id.rent_price,'contract_id':self.contract_id.id,'owner_id':self.owner_id.id,'renewal_date':self.renewal_date,'partner_id':self.renter_id.id,'property_id':self.property_id.id,'date':fields.Date.today(), 'from_date':self.from_date, 'to_date':self.to_date,'state':state})
+        contract_id = self.env['contract.details'].create(
+            {'contract_month': self.month, 'deposite': self.deposite_amount, 'renewal_date': self.renewal_date,
+             'rent_price': self.property_id.rent_price, 'contract_id': self.contract_id.id,
+             'owner_id': self.owner_id.id, 'renewal_date': self.renewal_date, 'partner_id': self.renter_id.id,
+             'property_id': self.property_id.id, 'date': fields.Date.today(), 'from_date': self.from_date,
+             'to_date': self.to_date, 'state': state})
         if contract_id:
-            self.property_id.write({'renter_history_ids':[(0,0,{
-                'contract_month':self.month,
-                'deposite':self.deposite_amount,
-                'reference':self.contract_id.name,
-                'property_id':self.property_id.id,
-                'owner_id':self.owner_id.id, 
-                'state':self.state, 
-                'rent_price':self.rent_price, 
-                'renter_id':self.renter_id.id, 
-                'date':fields.Date.today(), 
-                'from_date':self.from_date, 
-                'to_date':self.to_date, 
-                'property_id':self.property_id.id,
+            self.property_id.write({'renter_history_ids': [(0, 0, {
+                'contract_month': self.month,
+                'deposite': self.deposite_amount,
+                'reference': self.contract_id.name,
+                'property_id': self.property_id.id,
+                'owner_id': self.owner_id.id,
+                'state': self.state,
+                'rent_price': self.rent_price,
+                'renter_id': self.renter_id.id,
+                'date': fields.Date.today(),
+                'from_date': self.from_date,
+                'to_date': self.to_date,
+                # 'property_id':self.property_id.id,
                 'contract_id': contract_id.id,
             })]})
-            self.property_id.write({'state':'reserve','is_reserved':True,'user_id':self.env.user.id})
-            template_id =  self.env.ref('property_rental_mgt_app.property_reserved_template')
-            values = template_id.generate_email(self.id, ['subject', 'body_html', 'email_from', 'email_to', 'partner_to', 'email_cc', 'reply_to', 'scheduled_date'])
+            self.property_id.write({'state': 'reserve', 'is_reserved': True, 'user_id': self.env.user.id})
+            template_id = self.env.ref('property_rental_mgt_app.property_reserved_template')
+            values = template_id.generate_email(self.id,
+                                                ['subject', 'body_html', 'email_from', 'email_to', 'partner_to',
+                                                 'email_cc', 'reply_to', 'scheduled_date'])
             mail_mail_obj = self.env['mail.mail']
             msg_id = mail_mail_obj.sudo().create(values)
             if msg_id:
@@ -104,8 +117,8 @@ class PropertyBook(models.TransientModel):
             'name': 'Contract Details',
             'type': 'ir.actions.act_window',
             'view_mode': 'tree,form',
-            'res_id':contract_id.id,
+            'res_id': contract_id.id,
             'views': [(self.env.ref('property_rental_mgt_app.property_contract_details_form').id, 'form')],
             'res_model': 'contract.details',
-            'domain': [('invoice_id','=',self.property_id.id)],
+            'domain': [('invoice_id', '=', self.property_id.id)],
         }
